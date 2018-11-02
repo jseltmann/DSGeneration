15039749a258ca7ff2be3d532954dee35bd5f0aa
Johann Seltmann <johann.seltmann@gmx.net>
2018-11-02 19:07:08 +0100

    added code for pca

--- read_bigram_matrix.py
+++ read_bigram_matrix.py
@@ -2,9 +2,10 @@ import re
 import numpy as np
 import nltk
 import pickle
+from sklearn.decomposition import PCA
 
 
-def read_bigram_matrix(corpus_filename, freqdist_filename, out_filename, word_num=None):
+def read_bigram_matrix(corpus_filename, freqdist_filename, out_filename, word_num=None, stopwords=[]):
     """
     Read bigram model from a corpus and write it to a file.
     Model is saved as a dict with words as keys and vectors of probabilities as entries.
@@ -27,6 +28,8 @@ def read_bigram_matrix(corpus_filename, freqdist_filename, out_filename, word_nu
     word_num : Int
         Number of words to use. 
         If it is None, all the words in the frequency distribution file are used.
+    stopwords : [String]
+        words to ignore
     """
     matrix = dict()
     vector_order = dict()
@@ -43,6 +46,10 @@ def read_bigram_matrix(corpus_filename, freqdist_filename, out_filename, word_nu
                 print(line)
                 continue
             word = m.group(1).lower()
+
+            if word in stopwords:
+                continue
+            
             matrix[word] = None
             vector_order[word] = i
             i += 1
@@ -86,10 +93,50 @@ def read_bigram_matrix(corpus_filename, freqdist_filename, out_filename, word_nu
         pickle.dump(vector_order, orderfile)
 
 
+def pca_matrix(input_filename, output_filename):
+
+    with open(input_filename, "rb") as input_file:
+        matrix_dict = pickle.load(input_file)
+
+    vector_order_filename = input_filename + "_order"
+    with open(vector_order_filename, "rb") as vector_order_file:
+        vector_order = pickle.load(vector_order_file)
+
+    word_num = len(vector_order)
+    matrix = np.empty((word_num, word_num), float)
+
+    for word in matrix_dict:
+        word_pos = vector_order[word]
+        matrix[word_pos] = matrix_dict[word]
+
+    pca = PCA(n_components=300)
+    pca.fit(matrix)
+    transformed_matrix = pca.transform(matrix)
+
+    transformed_dict = dict()
+
+    for word in vector_order:
+        word_pos = vector_order[word]
+        transformed_dict[word] = transformed_matrix[word_pos]
+
+    with open(output_filename, "wb") as output_file:
+        pickle.dump(transformed_dict, output_file)
+    
+
+
             
 
+stopwords = [
+    "", "(", ")", "a", "about", "an", "and", "are", "around", "as", "at",
+    "away", "be", "become", "became", "been", "being", "by", "did", "do",
+    "does", "during", "each", "for", "from", "get", "have", "has", "had", "he",
+    "her", "his", "how", "i", "if", "in", "is", "it", "its", "made", "make",
+    "many", "most", "not", "of", "on", "or", "s", "she", "some", "that", "the",
+    "their", "there", "this", "these", "those", "to", "under", "was", "were",
+    "what", "when", "where", "which", "who", "will", "with", "you", "your"
+]
         
-read_bigram_matrix("../bnc_sentences", "../bnc_freqdist_lowercase.txt", "../bigram_matrix_10000.pkl", word_num=10000)
+#read_bigram_matrix("../bnc_sentences", "../bnc_freqdist_lowercase.txt", "../bigram_matrix_10000_stopwords.pkl", word_num=10000, stopwords=[])
         
-
+pca_matrix("../bigram_matrix_10000.pkl", "../bigram_matrix_10000_pca_300.pkl")
         
